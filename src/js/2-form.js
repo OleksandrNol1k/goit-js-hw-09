e18e135fa7ea90d9aa371fda585f1944e1bb0ebe
OleksandrNol1k <nevskij.mail@gmail.com>
2025-03-29 19:21:03 +0200

    done

---
+++ src/js/2-form.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+let formData = { email: "", message: "" };
+
+form.addEventListener("input", (event) => {
+  if (event.target.name in formData) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  }
+});
+
+const savedData = localStorage.getItem(localStorageKey);
+if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email || "";
+    form.elements.message.value = formData.message || "";
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!formData.email || !formData.message) { 
+        alert("Fill all fields.");
+        return;
+    }
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    form.reset()
+});
